061213fd9b8a23d600d89d7b9e8ea967cf91a156
Syed Hasnain Ali Shah <syedhasnainali927@gmail.com>
2024-08-21 09:43:04 +0500

    Update

--- TaskC.ts
+++ TaskC.ts
@@ -1,6 +1,6 @@
 // 100 Days Of Coding Challeng!
 
-/** Question 
+/** Question 69:
 Dividing and Finding the Remainder: Write a function that divides two numbers and returns both the quotient and the remainder.
 Use an object to return both values.*/
 
